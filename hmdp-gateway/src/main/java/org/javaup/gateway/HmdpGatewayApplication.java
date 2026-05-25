28250b066f50a034c930bc00dc493ba82446d451
LRB <3024414978@qq.com>
2026-05-25 14:58:26 +0800

    Add gateway module skeleton

---
+++ hmdp-gateway/src/main/java/org/javaup/gateway/HmdpGatewayApplication.java
@@ -0,0 +1,12 @@
+package org.javaup.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmdpGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HmdpGatewayApplication.class, args);
+    }
+}
